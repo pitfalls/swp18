252934c2cde1683d8d988af9839b13e12b36c90e
Alexander Frewein <alexander.frewein@student.tugraz.at>
2018-05-18 12:46:45 +0200

    Updated scala programm

--- Ass2/src/counter.scala
+++ Ass2/src/counter.scala
@@ -1,10 +1,16 @@
 object Demo {
    def main(args: Array[String]) {
-      println( "Returned Value : " + countName(1,List(1, 3, 2, 1, 1, 3)) );
+      println( "Returned Value : " + countName(1,List()) );
    }
    
     def countName(needle:Int, haystack: List[Int]) : Int = {
           var sum = 0
+          if(haystack == Nil)
+          {
+            println("Empty List");   
+            return 0; 
+          }
+
           for(i <- haystack)
           {
                if(i == needle)
