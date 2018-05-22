1f33ffb0b63458c1e2651fa2d22da6659ac6f4d0
Alexander Frewein <alexander.frewein@student.tugraz.at>
2018-05-22 18:50:54 +0200

    changed counter function, Started with beispiel 3

--- Ass2/src/counter.scala
+++ Ass2/src/counter.scala
@@ -1,10 +1,16 @@
 object Demo {
    def main(args: Array[String]) {
-      println( "Returned Value : " + countName(1,List()) );
+      println( "Returned Value : " + countName(1,List(1, 1, 3, 2)) );
    }
    
-    def countName(needle:Int, haystack: List[Int]) : Int = {
-          var sum = 0
+    def countName(needle:Int, haystack: List[Int]) : Int = haystack match{
+          case Nil => 0
+          case x::xs if(x == needle) => 1 + countName(x, xs)
+          case x::xs if(x != needle) => 0
+
+    
+  
+         /* var sum = 0
           if(haystack == Nil)
           {
             println("Empty List");   
@@ -19,5 +25,7 @@ object Demo {
                }
           }
           return sum
-     }    
+          */ 
+     }
+        
 }
